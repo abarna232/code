64f62afba58b098fe31df386cc4fe72fd287c619
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-02-15 15:56:14 +0530

    guvi98.c

---
+++ guvi98.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],n,i,j,t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i==1)
+        {
+            printf("%d",a[i]);
+        }
+    }
+}
