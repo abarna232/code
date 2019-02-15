437c573c81506fc4e77e8e79344d07e95e9f42b4
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-02-15 16:02:57 +0530

    guvi106.c

---
+++ guvi106.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],n,i,k,c=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    if(a[i]%2!=0)
+    {
+        c++;
+        if(c==k)
+        {
+            printf("%d",a[i]);
+        }
+    }
+    }
+    
+}
